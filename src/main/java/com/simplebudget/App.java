20130ff0865f9503a6d8467b9372d7f7299cbef4
Maria Jansson <mj225er@student.lnu.se>
2025-10-11 17:00:18 +0200

    Refactor App.java to improve code clarity and consistency; update scene dimensions and application title

--- src/main/java/com/simplebudget/App.java
+++ src/main/java/com/simplebudget/App.java
@@ -10,17 +10,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
-        stage.setTitle("SimpleBudget");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main-view.fxml"));
+        Scene scene = new Scene(loader.load());
         stage.setScene(scene);
+        stage.setTitle("Simple Budget App");
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
+
