fd7f6762c7842797e23929900f18dd3f4798596b
Maria Jansson <mj225er@student.lnu.se>
2025-10-13 21:00:06 +0200

    Add basic UI, update .gitignore with code counter, add a few methods to test AppController

--- src/main/java/com/simplebudget/AppController.java
+++ src/main/java/com/simplebudget/AppController.java
@@ -6,6 +6,55 @@
 
 package com.simplebudget;
 
+import java.util.ArrayList;
+
+import budgettracker.TransactionCategories;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
 public class AppController {
-  
+  @FXML
+  private Pane popupPane;
+  @FXML
+  private TextArea transactionListTextArea;
+  @FXML
+  private TextField idField;
+  @FXML
+  private TextField nameField;
+  @FXML
+  private TextField amountField;
+  @FXML
+  private ChoiceBox<TransactionCategories> transactionCategoryChoiceBox;
+
+  @FXML
+  private void initialize() {
+    transactionCategoryChoiceBox.getItems().addAll(TransactionCategories.values());
+  }
+
+  @FXML
+  private void listTransactions(ArrayList<String> transactionList) {
+    for (String transaction : transactionList) {
+      transactionListTextArea.appendText(transaction);
+    }
+  }
+
+  @FXML
+  private void openPopup() {
+    popupPane.setVisible(true);
+  }
+
+  @FXML
+  private void closePopup() {
+    popupPane.setVisible(false);
+  }
+
+  @FXML
+  private void closeApp(ActionEvent event) {
+    System.exit(0);
+  }
+
 }
